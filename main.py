7495507961f7d55c8492fdef6d5be0fef73a1053
Radu Sabina <91501977+radusabina@users.noreply.github.com>
2021-10-20 19:23:20 +0300

    Partial

--- main.py
+++ main.py
@@ -26,8 +26,44 @@ def test_numere_negative_nenule():
     assert numere_negative_nenule([-4, - 10, -56]) == [-4, -10, -56]
 
 
+def este_prim(n):
+    """
+    Determina daca un numar este prim
+    :param n: numarul
+    :return: True, daca numarul este prim, False in caz contrar
+    """
+    if n < 2:
+        return False
+    for i in range(2, n // 2+ 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def test_este_prim():
+    assert este_prim(3) is True
+    assert este_prim(10) is False
+
+
+def superprim(l):
+    rezultat = []
+    for i in l:
+        j = i
+        if j != 0:
+            if este_prim(j):
+                j = i % 10
+    rezultat.append(i)
+    return rezultat
+
+
+def test_superprim():
+    assert superprim([173, 239]) == [239]
+
+
 def test_all():
     test_numere_negative_nenule()
+    test_este_prim()
+    test_superprim()
 
 
 def main():
@@ -37,6 +73,7 @@ def main():
     while True:
         print("1. Citire lista")
         print("2. Afișarea tuturor numerelor negative nenule din listă")
+        print("4. Afișarea tuturor numerelor din listă care sunt superprime.")
         print("a. Afisare lista")
         print("x. Iesire")
         optiune = input("Introduceti optiunea: ")
@@ -44,6 +81,8 @@ def main():
             l = citire_lista(l)
         elif optiune == "2":
             print(numere_negative_nenule(l))
+        elif optiune == "4":
+            print(superprim(l))
         elif optiune.lower() == "a":
             print(l)
         elif optiune.lower() == "x":
